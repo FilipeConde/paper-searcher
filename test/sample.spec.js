42ecebfcfd8505e590463009c97758f2adcbb4a0
FilipeConde <filipecondep@gmail.com>
2021-11-06 20:37:40 -0300

    Configure Mocha and Chai - working fine

---
+++ test/sample.spec.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+
+const expect = chai.expect;
+
+describe('sample test', () => {
+  it('should run sample test', () => {
+    let aux = 2;
+    expect(2).eq(aux);
+  });
+});
