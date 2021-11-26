51e2bcab5afcc73a96ef3dd2b250ea33b38f0a13
FilipeConde <filipecondep@gmail.com>
2021-11-25 23:39:14 -0300

    Creates todo.md file

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,13 @@
 export default class Actions {
   static async type(page, locator, text) {
-    return page.type(locator, text);
+    page.type(locator, text);
   }
 
   static async getTextFromElement(page, locator) {
     return page.$eval(locator, (element) => element.textContent);
+  }
+
+  static async click(page, locator) {
+    page.click(locator);
   }
 }
---
+++ todo.md
@@ -0,0 +1,6 @@
+# CheckList
+
+- Acessar site Lume
+- Realizar pesquisa genérica
+- Ler dados do primeiro resultado
+- Salvar dados do primeiro resultado
